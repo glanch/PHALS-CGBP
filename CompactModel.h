36136ca47a1b13af07206c469a380faae07fca4f
glanch <49610590+glanch@users.noreply.github.com>
2023-06-13 00:11:18 +0200

    Add basic structure of comapct model

---
+++ CompactModel.h
@@ -0,0 +1,59 @@
+// CompactModel.h
+#pragma once
+
+#include "Instance.h"
+
+/* scip includes */
+#include "objscip/objscip.h"
+#include "objscip/objscipdefplugins.h"
+#include <memory>
+#include <map>
+using namespace std::map
+using namespace scip;
+
+class CompactModel
+{
+
+public:
+   // constructor
+   CompactModel(shared_ptr<Instance> instance);
+
+   // destructor
+   ~CompactModel();
+
+   // solve the problem
+   void Solve();
+
+   // display the solution
+   void DisplaySolution();
+
+   // set all optional SCIP-Parameters
+   void SetSCIPParameters();
+
+private:
+   shared_ptr<Instance> instance_;
+   SCIP* scip_;
+
+   // sentinels: start and end coils
+   Coil start_coil_
+   Coil end_coil_;
+
+   // variables
+   map<tuple<Coil, Coil, ProductionLine, Mode, Mode>>, SCIP_VAR*> vars_X_;
+
+   map<Coil, SCIP_VAR*> vars_Z_;
+
+   map<Coil, SCIP_VAR*> vars_S_;
+   
+   // constraints
+   map<Coil, SCIP_CONS*> cons_coil_partitioning_;
+   map<ProductionLine, SCIP_CONS*> cons_production_line_start_;
+
+   map<ProductionLine, SCIP_CONS*> cons_production_line_end_;
+
+   map<tuple<ProductionLine, Coil, Mode>, SCIP_CONS*> cons_flow_conservation_;
+
+   map<Coil, SCIP_CONS*> cons_delay_linking_;
+
+   map<pair<Coil, Coil>, SCIP_CONS*> cons_start_time_linking_;
+};
