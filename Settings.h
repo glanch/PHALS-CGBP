fe75a95f4dc20403643bb1933cb0f32f2bcfbfeb
glanch <49610590+glanch@users.noreply.github.com>
2023-07-08 02:07:48 +0200

    Add missing file

--- Settings.h
+++ Settings.h
@@ -6,6 +6,9 @@ namespace Settings
     constexpr double kBigM = 1000;
     constexpr double kDynamicGap = 50;
     constexpr double kDynamicGapLowerBound = 0.01;
+    constexpr double kDynamicGapTimeLimitInSeconds =  1e+20;
     constexpr bool kGenerateInitialTrivialColumn = true;
 
+    constexpr double kInitialSolveGap = 0;
+    constexpr double kInitialSolveTimeTimeLimitInSeconds = 1;
 }
