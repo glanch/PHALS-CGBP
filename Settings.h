8942142d55b841d8f377f2e33c45683e829cb635
glanch <49610590+glanch@users.noreply.github.com>
2023-07-08 13:00:09 +0200

    Add settings for previous commit

--- Settings.h
+++ Settings.h
@@ -4,11 +4,15 @@ namespace Settings
 {
     constexpr int kSCIPMaxStringLength = 1024;
     constexpr double kBigM = 1000;
+    
+    constexpr bool kGenerateInitialTrivialColumn = true;
+
+    constexpr double kDynamicGapMaxRounds = 5;
     constexpr double kDynamicGap = 50;
     constexpr double kDynamicGapLowerBound = 0.01;
     constexpr double kDynamicGapTimeLimitInSeconds =  1e+20;
-    constexpr bool kGenerateInitialTrivialColumn = true;
 
     constexpr double kInitialSolveGap = 0;
-    constexpr double kInitialSolveTimeTimeLimitInSeconds = 1;
+    constexpr double kInitialSolveTimeTimeLimitInSeconds = 0;
+    constexpr bool kInitialSolveEnabled = false;
 }
